49db0e5f7ff88622c4accf90c2fea4dc6727e55a
Md Mehedi Hasan <89269962+mehedih99@users.noreply.github.com>
2026-08-08 20:58:54 +0400

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-window.GOYA_CONFIG = {
-  // Paste your deployed Google Apps Script Web App /exec URL here.
-  API_URL: "",
-  COMPANY_NAME: "Goya",
-  COMPANY_SUBTITLE: "Operations Portal V2",
-  DEFAULT_ADMIN_PIN: "9988",
-  ENABLE_DEMO_MODE: true
-};
