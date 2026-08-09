a7cfe5ebc4e5919c583e4ce09cc0168bda9d8aca
Md Mehedi Hasan <89269962+mehedih99@users.noreply.github.com>
2026-08-09 09:47:56 +0400

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.GOYA_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxnucKSHDOhf8B7XRU1cr61sCQFsX9ItXfwNTCavqi8JRrCTrRW7aDY9-AMQANY2XEnMA/exec",
-  COMPANY_NAME: "Goya",
-  COMPANY_SUBTITLE: "Operations Portal V2",
-  DEFAULT_ADMIN_PIN: "9988"
-};
